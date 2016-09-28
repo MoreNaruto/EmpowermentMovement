edf5d7ab5309f9783e23d303d759805d107406d9
Thomas Morris <thomasmorris@Thomass-MacBook-Pro.local>
2016-09-28 13:36:05 -0500

    Implement gulp with javascript frameworks

---
+++ src/webapp/js/controller/events.js
@@ -0,0 +1,7 @@
+var eventController = angular.module('empowermentMovement.eventController', []);
+
+eventController
+	.controller('EventController', function($scope) {
+
+	}
+)
---
+++ src/webapp/test/controller/events_spec.js
@@ -0,0 +1,9 @@
+describe ('EventController', function() {
+	beforeEach(module('empowermentMovement.eventController'));
+
+	var $controller;
+
+	beforeEach(inject(function(_$controller_){
+		$controller = _$controller_;
+	}));
+})
