228222c874a747edcc6aad8515d6a3a1c2bb19ab
Thomas Morris <thomasmorris@Thomass-MacBook-Pro.local>
2016-09-29 15:04:25 -0500

    Adding a test directory

---
+++ src/main/test/com/empowermentapp/EventTest.java
@@ -0,0 +1,7 @@
+package com.empowermentapp;
+
+import static org.junit.Assert.*;
+
+public class EventTest {
+
+}
--- src/webapp/js/app.js
+++ src/webapp/js/app.js
@@ -1,6 +1,8 @@
 var EventController = require('./controller/events.js');
 
-var app = angular.module('EmpowermentMovement', []);
+var app = angular.module('EmpowermentMovement', [
+	'EmpowermentMovement.EventController'
+	]);
 
 app
 	.controller('EventController', ['$scope', EventController]);
